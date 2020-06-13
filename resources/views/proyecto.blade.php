a0e4ad320954ec02db715816e97c87fc6aa7fbcb
Vittoriano <vectorsjf@gmail.com>
2020-06-13 07:21:18 -0400

    cam

--- resources/views/proyecto.blade.php
+++ resources/views/proyecto.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width,user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0" >
 <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 
